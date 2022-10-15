b359b97947b2903883e7e780862b47d2db555451
fabrizio.deramo <fabrizio.deramo@PC-422-NB>
2022-10-15 12:17:47 -0300

    Agregar archivos de proyecto.

---
+++ ComercioApp/Controllers/DepositoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ComercioApp.Models;
+
+namespace ComercioApp.Controllers
+{
+    public class DepositoController : Controller
+    {
+        private ComercioEntities db = new ComercioEntities();
+
+        // GET: Deposito
+        public ActionResult Index()
+        {
+            return View(db.Deposito.ToList());
+        }
+
+        // GET: Deposito/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deposito deposito = db.Deposito.Find(id);
+            if (deposito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deposito);
+        }
+
+        // GET: Deposito/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Deposito/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CodigoDeposito,Descripcion")] Deposito deposito)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Deposito.Add(deposito);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(deposito);
+        }
+
+        // GET: Deposito/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deposito deposito = db.Deposito.Find(id);
+            if (deposito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deposito);
+        }
+
+        // POST: Deposito/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CodigoDeposito,Descripcion")] Deposito deposito)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deposito).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(deposito);
+        }
+
+        // GET: Deposito/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deposito deposito = db.Deposito.Find(id);
+            if (deposito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deposito);
+        }
+
+        // POST: Deposito/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Deposito deposito = db.Deposito.Find(id);
+            db.Deposito.Remove(deposito);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ComercioApp/Controllers/ProductoController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ComercioApp.Models;
+
+namespace ComercioApp.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private ComercioEntities db = new ComercioEntities();
+
+        // GET: Producto
+        public ActionResult Index()
+        {
+            var producto = db.Producto.Include(p => p.Deposito).Include(p => p.Proveedor);
+            return View(producto.ToList());
+        }
+
+        // GET: Producto/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Create
+        public ActionResult Create()
+        {
+            ViewBag.CodigoDeposito = new SelectList(db.Deposito, "CodigoDeposito", "Descripcion");
+            ViewBag.CodigoProveedor = new SelectList(db.Proveedor, "CodigoProveedor", "RazonSocial");
+            return View();
+        }
+
+        // POST: Producto/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NumeroSerie,Nombre,Cantidad,Precio,CodigoDeposito,CodigoProveedor")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Producto.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CodigoDeposito = new SelectList(db.Deposito, "CodigoDeposito", "Descripcion", producto.CodigoDeposito);
+            ViewBag.CodigoProveedor = new SelectList(db.Proveedor, "CodigoProveedor", "RazonSocial", producto.CodigoProveedor);
+            return View(producto);
+        }
+
+        // GET: Producto/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CodigoDeposito = new SelectList(db.Deposito, "CodigoDeposito", "Descripcion", producto.CodigoDeposito);
+            ViewBag.CodigoProveedor = new SelectList(db.Proveedor, "CodigoProveedor", "RazonSocial", producto.CodigoProveedor);
+            return View(producto);
+        }
+
+        // POST: Producto/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NumeroSerie,Nombre,Cantidad,Precio,CodigoDeposito,CodigoProveedor")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CodigoDeposito = new SelectList(db.Deposito, "CodigoDeposito", "Descripcion", producto.CodigoDeposito);
+            ViewBag.CodigoProveedor = new SelectList(db.Proveedor, "CodigoProveedor", "RazonSocial", producto.CodigoProveedor);
+            return View(producto);
+        }
+
+        // GET: Producto/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Producto producto = db.Producto.Find(id);
+            db.Producto.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
